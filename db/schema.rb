a70ca186d54e5b7fd9d0a0ca3bac5f3d8d80a4c2
baru-web-production <baru.web.production@gmail.com>
2021-01-11 20:24:04 +0900

    MYSQLの細かなバグを修正する

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_01_10_024405) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
