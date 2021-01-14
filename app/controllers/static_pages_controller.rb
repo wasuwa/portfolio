d271055f35e144948a68a8bc2143c2b16496a00d
baru-web-production <baru.web.production@gmail.com>
2021-01-14 19:00:41 +0900

    プロフィールページをRESTfulなリソースにする

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,24 +1,32 @@
 class StaticPagesController < ApplicationController
+
   def home
   end
+
   def account_setting
   end
+
   def password_setting
   end
+
   def login
   end
+
   def email
   end
-  def profile
-  end
+
   def other_profile
   end
+
   def new_articles_list
   end
+
   def favorite_list
   end
+
   def article_details
   end
+  
   def my_article_details
   end
 end
