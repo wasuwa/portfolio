a215abe595333cf8ba13e23b4cfd69c584817166
baru-web-production <baru.web.production@gmail.com>
2021-01-14 15:55:18 +0900

    userモデルのバリデーションにテストを追加する

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,49 +4,52 @@ RSpec.describe User, type: :model do
   let(:user) {
     User.new(
       name: "example",
-      email: "example@gmail.com"
-    )
-  }
-  let(:no_name) {
-    User.new(
-      name: "",
-      email: "no_name@gmail.com"
+      email: "example@example.com"
     )
   }
 
-  context "Name and email address exist" do
+  context 'when user is valid' do
     it 'valid' do
-      user.valid?
       expect(user).to be_valid
     end
-    it 'no names' do
-      no_name.valid?
-      expect(no_name.errors[:name]).to include("can't be blank")
+    it 'no name' do
+      user.name = ""
+      expect(user).to be_invalid
+    end
+    it 'no email' do
+      user.email = ""
+      expect(user).to be_invalid
     end
   end
 
-  # it "is invalid without email" do
+  context "when user's email needs to be unique" do
+    it 'valid' do
+      duplicate_user = user.dup
+      duplicate_user.email = user.email.upcase
+      user.save!
+      expect(duplicate_user).to be_invalid
+    end
+  end
 
-  # end
-  # it "is invalid if email is duplicated" do
-    
-  # end
-  # describe "word count" do
-  #   context "valid" do
-  #     it "is valid when name is 8 characters" do
-    
-  #     end
-  #     it "is valid when email is 255 characters" do
-    
-  #     end
-  #   end
-  #   context "invalid" do
-  #     it "is invalid when name is 9 characters" do
-    
-  #     end
-  #     it "is invalid when email is 256 characters" do
-    
-  #     end
-  #   end
-  # end
+  context 'name length' do
+    it '8 characters' do
+      user.name = 'a' * 8
+      expect(user).to be_valid
+    end
+    it '9 characters' do
+      user.name = 'a' * 9
+      expect(user).to be_invalid
+    end
+  end
+
+  context 'email length' do
+    it '255 characters' do
+      user.email = 'a' * 243 + '@example.com'
+      expect(user).to be_valid
+    end
+    it '256 characters' do
+      user.email = 'a' * 244 + '@example.com'
+      expect(user).to be_invalid
+    end
+  end
 end
