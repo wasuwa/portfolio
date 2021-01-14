c2750c34a09ba90979563b068c09a19d5286220f
baru-web-production <baru.web.production@gmail.com>
2021-01-15 04:58:26 +0900

    新規登録ページのformを作成する

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "users/new.html.erb", type: :view do
-  
+    # let(:user) {
+    #     User.new(
+    #     name: "example",
+    #     email: "example@example.com",
+    #     password: "password",
+    #     password_confirmation: "password",
+    #     grade: 2
+    #     )
+    # }
+
+    # context 'show the correct name on your profile' do
+    #     it 'valid' do
+    #         expect(user.name).to eq ''
+    #     end
+    # end
 end
