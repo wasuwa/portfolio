ab669bb09921b74167ccfd6aaebef14cb26fd02c
baru-web-production <baru.web.production@gmail.com>
2021-02-03 21:26:18 +0900

    お気に入り数のTOP3を表示する機能を追加する

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
 
   def home
     @articles = Article.all
+    @all_ranks = Article.find(Favorite.group(:article_id).order('count(article_id) desc').limit(3).pluck(:article_id))
   end
 
   def email
