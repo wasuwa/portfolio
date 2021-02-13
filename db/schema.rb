d9b82bd8614eb59d2924d66c210954f6978548b4
baru-web-production <baru.web.production@gmail.com>
2021-02-13 20:04:52 +0900

    config.ymlでwebpackerをインストールする

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20_210_204_110_144) do
-  create_table "articles", :charset => "utf8mb4", :collation => "utf8mb4_unicode_ci", :force => :cascade do |t|
+ActiveRecord::Schema.define(version: 2021_02_04_110144) do
+
+  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.text "content"
-    t.bigint "user_id", :null => false
-    t.datetime "created_at", :precision => 6, :null => false
-    t.datetime "updated_at", :precision => 6, :null => false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.string "image"
-    t.index ["user_id", "created_at"], :name => "index_articles_on_user_id_and_created_at"
-    t.index ["user_id"], :name => "index_articles_on_user_id"
+    t.index ["user_id", "created_at"], name: "index_articles_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "comments", :charset => "utf8mb4", :collation => "utf8mb4_unicode_ci", :force => :cascade do |t|
+  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "article_id"
-    t.datetime "created_at", :precision => 6, :null => false
-    t.datetime "updated_at", :precision => 6, :null => false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "content"
   end
 
-  create_table "favorites", :charset => "utf8mb4", :collation => "utf8mb4_unicode_ci", :force => :cascade do |t|
+  create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "article_id"
-    t.datetime "created_at", :precision => 6, :null => false
-    t.datetime "updated_at", :precision => 6, :null => false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", :charset => "utf8mb4", :collation => "utf8mb4_unicode_ci", :force => :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", :precision => 6, :null => false
-    t.datetime "updated_at", :precision => 6, :null => false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.integer "grade"
     t.string "remember_digest"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.string "icon"
-    t.index ["email"], :name => "index_users_on_email", :unique => true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "articles", "users"
