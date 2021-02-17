b0f0d609e14400bcfca3b1e5f7508a1eddd4cc9e
baru-web-production <baru.web.production@gmail.com>
2021-02-17 23:45:36 +0900

    unicorn.conf.rbをrubocopに従って修正する

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,16 +1,16 @@
-$worker  = 2
-$timeout = 30
-$app_dir = "/var/www/rails/portfolio"
-$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-$std_log = File.expand_path 'log/unicorn.log', $app_dir
-worker_processes  $worker
-working_directory $app_dir
-stderr_path $std_log
-stdout_path $std_log
-timeout $timeout
-listen  $listen
-pid $pid
+jobs = 2
+timeout = 30
+app_dir = "/var/www/rails/portfolio"
+listen  = File.expand_path 'tmp/sockets/.unicorn.sock', app_dir
+pid = File.expand_path 'tmp/pids/unicorn.pid', app_dir
+std_log = File.expand_path 'log/unicorn.log', app_dir
+worker_processes  jobs
+working_directory app_dir
+stderr_path std_log
+stdout_path std_log
+timeout timeout
+listen  listen
+pid pid
 preload_app true
 
 before_fork do |server, worker|
@@ -20,6 +20,7 @@ before_fork do |server, worker|
     begin
       Process.kill "QUIT", File.read(old_pid).to_i
     rescue Errno::ENOENT, Errno::ESRCH
+      return
     end
   end
 end
