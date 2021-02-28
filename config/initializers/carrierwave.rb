27d63089d8e1dd97e13801a7df87b107fcc2dfda
baru-web-production <baru.web.production@gmail.com>
2021-02-28 14:45:48 +0900

    carrierwave.rbにrequireを追加する

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,15 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
+      path_style: true
     }
 
     config.fog_directory = 'hssb-portfolio-s3'
