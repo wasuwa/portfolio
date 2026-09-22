34b7874cdb9360550b7bf86ec081b26be90ebc92
wasu <wasuwa@users.noreply.github.com>
2026-09-22 16:23:36 +0900

    biome init

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,10 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
